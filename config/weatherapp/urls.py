88913e902d17ee8637c65258947aafb5f238b360
Szade <wojciech.szade@gmail.com>
2023-10-11 21:27:47 +0200

    Ostatnie poprawki

--- config/weatherapp/urls.py
+++ config/weatherapp/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("cities", views.cities, name="cities"),
     path("oneday/<str:city_key>", views.oneday, name="oneday"),
     path("fiveday/<str:city_key>", views.fiveday, name="fiveday"),
+    path("cities_forecast", views.cities_forecast, name="cities_forecast"),
+    path("historical/<str:city_key>", views.historical, name="historical")
 ]
--- config/weatherapp/views.py
+++ config/weatherapp/views.py
@@ -8,17 +8,19 @@ api_key = config('API_KEY')
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<a href='cities'>Cities</a><a href='minute'>Minute</a>")
+    return HttpResponse("<a href='cities'>Cities</a></br><a href='cities_forecast'>Cities current conditions</a>")
 
 def cities(request):
-    # api call
     cities = requests.get("http://dataservice.accuweather.com/locations/v1/topcities/50", params={"apikey": api_key})
+    if not cities.ok:
+        return HttpResponse(cities)
     cities = cities.json()
     body = "<html><body> <h1>Top 50 Cities</h1> <ul>"
     for city in cities:
         body += "<li>" + city['EnglishName']
-        body += ": <a href='/oneday/" + city['Key'] + "'>One Day Forecast</a>"
-        body += ": <a href='/fiveday/" + city['Key'] + "'>Five Day Forecast</a>"
+        body += "; <a href='/oneday/" + city['Key'] + "'>One Day Forecast</a>"
+        body += "; <a href='/fiveday/" + city['Key'] + "'>Five Day Forecast</a>"
+        body += "; <a href='/historical/" + city['Key'] + "'>Historical 24h Forecast</a>"
         body += "</li>"
     
     body += "</ul></body></html>"
@@ -27,12 +29,15 @@ def cities(request):
 
 def oneday(request, city_key):
     forecast = requests.get("http://dataservice.accuweather.com/forecasts/v1/daily/1day/" + city_key, params={"apikey": api_key})
+    if not forecast.ok:
+        return HttpResponse(forecast)
+    forecast = forecast.json()
     body = "<html><body>"
     body += "<h1>One Day Forecast</h1>"
     body += "<ul>"
-    body += "<li>Headline: " + forecast.json()['Headline']['Text'] + "</li>"
-    body += "<li>Minimum Temperature: " + str(forecast.json()['DailyForecasts'][0]['Temperature']['Minimum']['Value']) + "</li>"
-    body += "<li>Maximum Temperature: " + str(forecast.json()['DailyForecasts'][0]['Temperature']['Maximum']['Value']) + "</li>"
+    body += "<li>Headline: " + forecast['Headline']['Text'] + "</li>"
+    body += "<li>Minimum Temperature: " + str((forecast)['DailyForecasts'][0]['Temperature']['Minimum']['Value']) + "</li>"
+    body += "<li>Maximum Temperature: " + str((forecast)['DailyForecasts'][0]['Temperature']['Maximum']['Value']) + "</li>"
     body += "</ul>"
     body += "</body></html>"
     return HttpResponse(body)
@@ -40,12 +45,47 @@ def oneday(request, city_key):
 
 def fiveday(request, city_key):
     forecast = requests.get("http://dataservice.accuweather.com/forecasts/v1/daily/5day/" + city_key, params={"apikey": api_key})
+    if not forecast.ok:
+        return HttpResponse(forecast)
+    forecast = forecast.json()
     body = "<html><body>"
     body += "<h1>Five Day Forecast</h1>"
     body += "<ul>"
-    body += "<li>Headline: " + forecast.json()['Headline']['Text'] + "</li>"
-    body += "<li>Minimum Temperature: " + str(forecast.json()['DailyForecasts'][0]['Temperature']['Minimum']['Value']) + "</li>"
-    body += "<li>Maximum Temperature: " + str(forecast.json()['DailyForecasts'][0]['Temperature']['Maximum']['Value']) + "</li>"
+    body += "<li>Headline: " + forecast['Headline']['Text'] + "</li>"
+    body += "<li>Minimum Temperature: " + str(forecast['DailyForecasts'][0]['Temperature']['Minimum']['Value']) + "</li>"
+    body += "<li>Maximum Temperature: " + str(forecast['DailyForecasts'][0]['Temperature']['Maximum']['Value']) + "</li>"
     body += "</ul>"
     body += "</body></html>"
     return HttpResponse(body) 
+
+def cities_forecast(request):
+    forecast_cities = requests.get("http://dataservice.accuweather.com/currentconditions/v1/topcities/50", params={"apikey": api_key})
+    if not forecast_cities.ok:
+        return HttpResponse(forecast_cities)
+    forecast_cities = forecast_cities.json()
+    body = "<html><body> <h1>Top 50 Cities</h1> <ul>"
+    for city in forecast_cities:
+        body += "<li>" + city['EnglishName'] + ": "
+        body += str(city['WeatherText']) + "</br>"
+        
+    body += "</ul></body></html>"
+    
+    return HttpResponse(body)
+
+
+def historical(request, city_key):
+    forecast = requests.get("http://dataservice.accuweather.com/currentconditions/v1/" + city_key + "/historical/24/", params={"apikey": api_key})
+    if not forecast.ok:
+        return HttpResponse(forecast)
+    forecast = forecast.json()
+    body = "<html><body>"
+    body += "<h1>Historical forecast</h1>"
+    body += "<ul>"
+    for hour in forecast:
+        body+= "<li>Time: " + str(hour['LocalObservationDateTime']) + "</li>"
+        body+= "<li>Weather: " + str(hour['WeatherText']) + "</li>"
+        body+= "<li>Temperature: " + str(hour['Temperature']['Metric']['Value']) + "</li>"
+    body += "</ul>"
+    body += "</body></html>"
+    return HttpResponse(body)
+
